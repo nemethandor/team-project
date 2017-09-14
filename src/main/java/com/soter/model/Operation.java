4763b89520c0d3f44b1c40f55d32c64d1f2f89d5
tozogabee <tozogabee@gmail.com>
2017-09-14 19:58:05 +0200

    Division method

--- src/main/java/com/soter/model/Operation.java
+++ src/main/java/com/soter/model/Operation.java
@@ -1,7 +1,20 @@
 package com.soter.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+import static java.math.RoundingMode.CEILING;
+
 /**
  * Created by java on 2017.09.14..
  */
 public class Operation {
+
+
+    public Double division(double number1, Double number2 ){
+        double a = Math.round(number1/number2*100000000)/100000000;
+        return a;
+    }
+
 }
